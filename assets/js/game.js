43b68c1650b982c6e248cbd438c110c7405ea3a5
Umair Ali <umairmamoor@gmail.com>
2021-07-05 14:53:54 -0400

    updated game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("our robot's name is " +playerName);
+var name = "your name";
+console.log(name);
+function fight(){
+    window.alert("The fight has begun");
+}
+//fight();
